0b299637213ad90d4defecc61607fb5228c50ba2
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2021-10-30 12:31:30 -0400

    added user input functionality

--- index.js
+++ index.js
@@ -12,3 +12,27 @@ const questions = [
     name: "Manager",
   },
 ];
+
+// Function to create the HTML file
+function writeToFile(fileName, data) {
+  fs.writeToFile(fileName, data, function (err) {
+    console.log(fileName);
+    console.log(data);
+    if (err) {
+      return console.log(err);
+    } else {
+      console.log("Yay! It worked!");
+    }
+  });
+}
+
+// Function that will initialize the application
+function init() {
+  inquirer.prompt(questions).then(function (data) {
+    writeToFile("index.html", generateHtml(data));
+    console.log(data);
+  });
+}
+
+//Function call to initialize application
+init();
